74fb3d0160385128c388375bdc014dc8507f91d4
Subodh Khanal <skhanal5@terpmail.umd.edu>
2023-08-03 21:01:06 -0700

    added resources route and updated sidebar

--- crud-app/app-tracker/app/components/SideBar.js
+++ crud-app/app-tracker/app/components/SideBar.js
@@ -83,14 +83,23 @@ export default function SideBar() {
           </ul>
         </div>
         <div className="flex flex-col rounded-lg bg-gradient-to-b from-blue-100 to-white p-5 justify-center items-center w-48">
-          <h2 className="text-blue-800 text-xs text-center font-bold">Stuck in your job search?</h2>
+          <h2 className="text-blue-800 text-xs text-center font-bold">
+            Stuck in your job search?
+          </h2>
           <p className="text-gray-700 text-xs text-center mt-1">
-             We can help you get started.
+            We can help you get started.
           </p>
-          <Image src="/help.svg" className="mt-5" width={100} height={300} ></Image>
-          <button className="bg-blue-600 hover:bg-blue-500 shadow-lg text-white font-semibold text-xs mt-5 py-2 px-4 rounded">
-            Free Resources
-          </button>
+          <Image
+            src="/help.svg"
+            className="mt-5"
+            width={100}
+            height={300}
+          ></Image>
+          <Link href="/resources">
+            <button className="bg-blue-600 hover:bg-blue-500 shadow-lg text-white font-semibold text-xs mt-5 py-2 px-4 rounded">
+              Free Resources
+            </button>
+          </Link>
         </div>
       </div>
     </div>
