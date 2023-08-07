9482c8ba18ae666faa376dcbbf48c01dabc561c4
Subodh Khanal <skhanal5@terpmail.umd.edu>
2023-08-06 18:27:45 -0700

    header is now dynamic

--- crud-app/app-tracker/app/components/Header.js
+++ crud-app/app-tracker/app/components/Header.js
@@ -1,15 +1,56 @@
+"use client";
+
 import { BellIcon, MoonIcon } from "@heroicons/react/24/outline";
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 
 export default function Header({ childComponent }) {
-  return (
-    <div>
-      <div className="flex justify-between">
-        <div className="flex flex-col">
+  const pathName = usePathname();
+
+  const renderHeader = () => {
+    if (pathName == "/dashboard") {
+      return (
+        <div>
           <h2 className="font-bold text-2xl font-blue-700"> Dashboard </h2>
           <p className="mt-1 text-sm font-light font-gray-100">
             Here are some quick insights for your job search.
           </p>
+        </div>
+      );
+    } else if (pathName == "/tracker") {
+      return (
+        <div>
+          <h2 className="font-bold text-2xl font-blue-700"> Tracker </h2>
+          <p className="mt-1 text-sm font-light font-gray-100">
+            Record, manage, and streamline your job applications.
+          </p>
+        </div>
+      );
+    } else if (pathName == "/goals") {
+      return (
+        <div>
+          <h2 className="font-bold text-2xl font-blue-700"> Goals</h2>
+          <p className="mt-1 text-sm font-light font-gray-100">
+            Define and track your desired achievements or objectives.
+          </p>
+        </div>
+      );
+    } else if (pathName == "/metrics") {
+      return (
+        <div>
+          <h2 className="font-bold text-2xl font-blue-700"> Metrics </h2>
+          <p className="mt-1 text-sm font-light font-gray-100">
+            Analyze in-depth insights for your progress via visualizations.
+          </p>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex justify-between">
+        <div className="flex flex-col">
+          {renderHeader()}
         </div>
         <div className="flex items-center gap-2">
           <div className="flex flex-row gap-5 justify-center items-center">
